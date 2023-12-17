7b71b2aafa8c89d06b5ac745462c4866cd9594c0
novaid12 <spikulik8558@icloud.com>
2023-12-17 16:23:43 +0300

    Created project

---
+++ ColorChanger/ColorChanger/ColorChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangerApp.swift
+//  ColorChanger
+//
+//  Created by  NovA on 17.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorChanger/ColorChanger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorChanger
+//
+//  Created by  NovA on 17.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
